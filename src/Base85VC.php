dcbb4c01e2732d0030a4c1c0725b266132a2b16f
ierusalim <alex@ierusalim.com>
2019-05-16 18:20:47 +0300

    In-Out Codepage parameter added

--- src/Base85VC.php
+++ src/Base85VC.php
@@ -5,30 +5,32 @@
     {
         public $bco;
 
-        public function base85vc_decode($src) {
+        public function base85vc_decode($src, $from_cp = 'utf-8')
+        {
             if (!strlen($src)) {
                 return '';
             }
-            $s1251 = \mb_convert_encoding($src, 'cp1251','utf-8');
+            $s1251 = \mb_convert_encoding($src, 'cp1251', $from_cp);
             $dec = $this->bco->basex_decode($s1251, 85);
             return hex2bin($this->bco->dectohex($dec));
         }
 
-        public function base85vc_encode($src)
+        public function base85vc_encode($src, $out_cp = 'utf-8')
         {
             $dec = $this->bco->hextodec(bin2hex($src));
             $r1251 = $this->bco->basex_encode($dec, 85);
-            return \mb_convert_encoding($r1251,'utf-8','cp1251');
+            return \mb_convert_encoding($r1251, $out_cp, 'cp1251');
         }
 
-        public function __construct() {
+        public function __construct()
+        {
             $this->bco = new \ierusalim\BaseConvert\BaseConvert(
                 85,
                     '0123456789'
                   . 'ABCDEFGHJKLMNPQRSTUVWXYZ'
                   . 'abcdefghijkmnopqrstuvwxyz'
-                  . \mb_convert_encoding('БГДЖИЛПФЦЧШЮЯ','cp1251','utf-8')
-                  . \mb_convert_encoding('згджилпфцчшюя','cp1251','utf-8')
+                  . \mb_convert_encoding('БГДЖИЛПФЦЧШЮЯ', 'cp1251', 'utf-8')
+                  . \mb_convert_encoding('згджилпфцчшюя', 'cp1251', 'utf-8')
                 );
         }
     }
